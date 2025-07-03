0916573565d630512eef24abf4310cbcf4636ee8
Shivam Dave <shivamndave@gmail.com>
2025-07-03 01:53:41 -0700

    Update base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/shivamndave.github.io/",
+  base: "https://shivamndave.github.io/shivamndave.github.io/",
 });
